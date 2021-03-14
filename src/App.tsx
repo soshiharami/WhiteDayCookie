78aca213ff292a08dcc6952696e8bd2ed8cf98c5
soshiharami <soshi0807sss@gmail.com>
2021-03-14 22:23:04 +0900

    feat: add cookie

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Cookies from "js-cookie";
 
 function App() {
+
+  Cookies.set("cookie","ずっと前から好きでした！付き合ってください！")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        クッキー焼いたんだ....
+      </h1>
+      <h1>
+        食べて....くれるかな？
+      </h1>
     </div>
   );
 }
